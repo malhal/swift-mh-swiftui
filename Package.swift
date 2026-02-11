e50bbbe8eb2caebf1104877bccb485249e574ce8
Malcolm Hall <malhal@users.noreply.github.com>
2026-02-11 16:50:41 +0000

    Update

--- Package.swift
+++ Package.swift
@@ -1,12 +1,19 @@
 // swift-tools-version: 5.9
+import Foundation
 import PackageDescription
+
+let isLocal = FileManager.default.fileExists(atPath: "../swift-mh-foundation")
 
 let package = Package(
     name: "swift-mh-swiftui",
     products: [
         .library(name: "MHSwiftUI", targets: ["MHSwiftUI"]),
     ],
-    dependencies: [
+    dependencies: isLocal ? [
+        // 1. If local folder exists, use it (No identity conflict)
+        .package(path: "../swift-mh-foundation")
+    ] : [
+        // 2. Otherwise, use the remote URL (Standalone mode)
         .package(url: "https://github.com/malhal/swift-mh-foundation.git", branch: "main")
     ],
     targets: [
